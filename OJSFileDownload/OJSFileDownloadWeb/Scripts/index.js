5b9e50b05f5316ca2c41a0c3b2640ebd55ea5521
hirossyi73 <it@hirossyi.net>
2016-02-08 14:26:05 +0900

    Create Samole.

---
+++ OJSFileDownload/OJSFileDownloadWeb/Scripts/index.js
@@ -0,0 +1,43 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // 新しいページが読み込まれるたびに初期化関数を実行する必要があります
+    //Office.initialize = function (reason)
+    {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#pdf_XmlHttpRequest').on('click', { file: 'sample.pdf' }, xmlHttpRequestDownload);
+            $('#png_XmlHttpRequest').on('click', { file: 'sample.png' }, xmlHttpRequestDownload);
+        });
+    };
+
+    function xmlHttpRequestDownload(event) {
+        var fileName = event.data.file;
+        var oReq = new XMLHttpRequest();
+        oReq.open("GET", fileName, true);
+        oReq.responseType = "blob";
+
+        oReq.onload = function (oEvent) {
+            var blob = oReq.response; // Note: not oReq.responseText
+            if (blob) {
+                saveFile(blob, fileName);
+            }
+        };
+
+        oReq.send(null);
+    }
+
+    function saveFile(blob, fileName) {
+        if (window.navigator.msSaveOrOpenBlob) {
+            window.navigator.msSaveOrOpenBlob(blob, fileName);
+        } else {
+            var link = document.createElement('a');
+            link.href = window.URL.createObjectURL(blob);
+            link.download = fileName;
+            link.click();
+        }
+    }
+})();
